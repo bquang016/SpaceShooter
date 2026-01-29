07beb384c0e01884870245a0d9307574a18b10a6
bquang016 <buidangquangmtp@gmail.com>
2026-01-29 17:44:25 +0700

    done part 3

---
+++ Assets/EnemyHealth.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public GameObject explosionPrefab; // Prefab hiệu ứng nổ
+
+    // Hàm này tự động chạy khi có vật thể (Trigger) bay vào
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        // Tạo hiệu ứng nổ (sẽ làm ở bước 5)
+        if (explosionPrefab != null)
+        {
+            var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(explosion, 1f); // Xóa hiệu ứng nổ sau 1 giây
+        }
+
+        // Xóa máy bay địch
+        Destroy(gameObject);
+    }
+}
--- Assets/PlayerShootingg.cs
+++ Assets/PlayerShootingg.cs
@@ -4,13 +4,14 @@ public class PlayerShootingg : MonoBehaviour
 {
     public GameObject bulletPrefabs;
     public float shootingInterval;
+    public Vector3 bulletOffset; // Biến mới thêm vào
     private float lastBulletTime;
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            if (Time.time - lastBulletTime >
-            shootingInterval)
+            if (Time.time - lastBulletTime > shootingInterval)
             {
                 ShootBullet();
                 lastBulletTime = Time.time;
@@ -18,8 +19,9 @@ public class PlayerShootingg : MonoBehaviour
         }
     }
 
-    private void ShootBullet()
+    void ShootBullet()
     {
-        Instantiate(bulletPrefabs, transform.position, transform.rotation);
+        // Cộng thêm bulletOffset vào vị trí hiện tại
+        Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
     }
 }
