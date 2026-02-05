cc0253ec093a96f5b5eee03c85b3702cb06a089d
bquang016 <buidangquangmtp@gmail.com>
2026-02-05 22:10:48 +0700

    done

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -1,25 +1,12 @@
 ﻿using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
+public class EnemyHealth : Health
 {
-    public GameObject explosionPrefab; // Prefab hiệu ứng nổ
 
-    // Hàm này tự động chạy khi có vật thể (Trigger) bay vào
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    protected override void Die()
     {
-        Die();
-    }
-
-    private void Die()
-    {
-        // Tạo hiệu ứng nổ (sẽ làm ở bước 5)
-        if (explosionPrefab != null)
-        {
-            var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
-            Destroy(explosion, 1f); // Xóa hiệu ứng nổ sau 1 giây
-        }
-
-        // Xóa máy bay địch
-        Destroy(gameObject);
+        base.Die(); 
+        Debug.Log("Enemy died"); 
     }
 }
---
+++ Assets/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public GameObject explosionPrefab; // Prefab nổ
+    public int defaultHealthPoint = 3; // Máu mặc định
+    private int healthPoint;
+
+    private void Start() => healthPoint = defaultHealthPoint;
+
+    public void TakeDamage(int damage)
+    {
+        if (healthPoint <= 0) return;
+        healthPoint -= damage;
+        if (healthPoint <= 0) Die();
+    }
+
+    protected virtual void Die()
+    {
+        // Tạo hiệu ứng nổ nếu có
+        if (explosionPrefab != null)
+        {
+            var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(explosion, 1f);
+        }
+        Destroy(gameObject);
+    }
+}
